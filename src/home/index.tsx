ae25561fb85a185d3df7a8b01c85318d383eed92
Mdromi <sadrahman.dev@gmail.com>
2024-12-04 13:28:14 +0600

    setup shadcn ui and react router

---
+++ src/home/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+function Home() {
+  return (
+    <>
+      <h1>AI Resume Builder</h1>
+      <Button>Click me</Button>
+    </>
+  );
+}
+
+export default Home;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SignInPage from "./auth/sign-in/index.tsx";
+import Dashboard from "./dashboard/index.tsx";
+import Home from "./home/index.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+    ],
+  },
+  {
+    path: "/auth/sign-in",
+    element: <SignInPage />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
